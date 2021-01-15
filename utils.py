f1bcc89fd0b6c4ff609e7f6913de8689a06c87f3
Sean <s7782519728@gmail.com>
2021-01-15 14:28:16 -0800

    Add typing

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import asyncio
 import gzip
-import typing
+from typing import List, Iterable, Generator, Dict
 
 import aiofiles
 import virtool_core.history.db
@@ -34,7 +34,15 @@ SEQUENCE_KEYS = [
 ]
 
 
-async def get_patched_otus(db, data_path, manifest):
+async def get_patched_otus(db, data_path: str, manifest: dict) -> List[dict]:
+    """
+    Get joined OTUs patched to a specific version based on a manifest of OTU ids and versions.
+
+    :param db: the Virtool database client
+    :param data_path: the Virtool data path
+    :param manifest: the manifest
+
+    """
     coros = list()
 
     for patch_id, patch_version in manifest.items():
@@ -48,12 +56,15 @@ async def get_patched_otus(db, data_path, manifest):
     return [j[1] for j in await asyncio.tasks.gather(*coros)]
 
 
-async def write_sequences_to_file(path, sequences):
+async def write_sequences_to_file(path: str, sequences: Iterable):
     """
     Write a FASTA file based on a given `Iterable` containing sequence documents.
+
     Headers will contain the `_id` field of the document and the sequence text is from the `sequence` field.
+
     :param path: the path to write the file to
     :param sequences: the sequences to write to file
+
     """
     async with aiofiles.open(path, "w") as f:
         for sequence in sequences:
@@ -65,17 +76,20 @@ async def write_sequences_to_file(path, sequences):
 
 
 def get_sequences_from_patched_otus(
-        otus: typing.Iterable[dict],
+        otus: Iterable[dict],
         data_type: str, sequence_otu_map: dict
-) -> typing.Generator[dict, None, None]:
+) -> Generator[dict, None, None]:
     """
     Return sequence documents based on an `Iterable` of joined OTU documents. Writes a map of sequence IDs to OTU IDs
     into the passed `sequence_otu_map`.
+
     If `data_type` is `barcode`, all sequences are returned. Otherwise, only sequences of default isolates are returned.
+
     :param otus: an Iterable of joined OTU documents
     :param data_type: the data type of the parent reference for the OTUs
     :param sequence_otu_map: a dict to populate with sequence-OTU map information
     :return: a generator that yields sequence documents
+
     """
     for otu in otus:
         otu_id = otu["_id"]
@@ -93,7 +107,7 @@ def get_sequences_from_patched_otus(
                 yield sequence
 
 
-async def export(db, data_path, ref_id):
+async def export(db, data_path: str, ref_id: str):
     otu_list = list()
 
     query = {
@@ -116,7 +130,7 @@ async def export(db, data_path, ref_id):
     return clean_export_list(otu_list)
 
 
-def clean_export_list(otus):
+def clean_export_list(otus: List[Dict]):
     cleaned = list()
 
     otu_keys = OTU_KEYS + ["_id"]
@@ -140,7 +154,7 @@ def clean_export_list(otus):
     return cleaned
 
 
-def clean_otu(otu, otu_keys=None, sequence_keys=None):
+def clean_otu(otu, otu_keys: List[str] = None, sequence_keys: List[str] = None):
     otu_keys = otu_keys or OTU_KEYS
     sequence_keys = sequence_keys or SEQUENCE_KEYS
 
